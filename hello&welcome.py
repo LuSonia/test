4e4d4ee6bb2f7a2c25ecab0a0255bccb02080df6
LuSonia <19098533i011008@student.must.edu.mo>
2022-03-28 11:31:17 +0800

    added heelo.py and welcome.py

---
+++ hello&welcome.py
@@ -0,0 +1,9 @@
+print("hello world")
+print("welcome to the new world")
+
+number=input("enter a number")
+if not(int(number)%2):
+    print("even")
+else:
+    print("odd")
+print("ok, now let us enter the new world")
